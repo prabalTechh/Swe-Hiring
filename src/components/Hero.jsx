e038ef0170d166d1ff5bfffbd19b810a16ce6a6f
Prabal Chanpuria <id.prabal@gmail.com>
2024-08-13 11:07:23 +0530

    First committed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function HeroSection() {
   return (
@@ -6,17 +6,14 @@ function HeroSection() {
       <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
-        Nike Air Max is a line of shoes produced by Nike, Inc., with the first model released in 1987. Air Max shoes are identified by their midsoles incorporating flexible urethane pouches filled with pressurized gas, visible from the exterior of the shoe and intended to provide cushioning to the underfoot
+          Nike Air Max is a line of shoes produced by Nike, Inc., with the first model released in 1987. Air Max shoes are identified by their midsoles incorporating flexible urethane pouches filled with pressurized gas, visible from the exterior of the shoe and intended to provide cushioning to the underfoot.
         </p>
-
         <div className="hero-btn">
-          <button>Shop Now </button>
+          <button>Shop Now</button>
           <button className="secondary-btn">Category</button>
         </div>
-
         <div className="shopping">
           <p>Also Available On</p>
-
           <div className="brand-icons">
             <img src="/images/amazon.png" alt="amazon-logo" />
             <img src="/images/flipkart.png" alt="flipkart-logo" />
@@ -25,10 +22,9 @@ function HeroSection() {
       </div>
       <div className="hero-image">
         <img src="/images/hero-image.png" alt="hero-image" />
-        
       </div>
     </main>
-  )
+  );
 }
 
 export default HeroSection;
